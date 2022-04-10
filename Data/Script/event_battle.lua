1169e8f631757385e0b627048213f77fa2da5871
audino <2676737+audinowho@users.noreply.github.com>
2022-04-10 12:25:43 -0800

    Refactor common tables

---
+++ Data/Script/event_battle.lua
@@ -0,0 +1,184 @@
+require 'common'
+
+BATTLE_SCRIPT = {}
+
+function BATTLE_SCRIPT.Test(owner, ownerChar, context, args)
+  PrintInfo("Test")
+end
+
+function BATTLE_SCRIPT.AllyInteract(owner, ownerChar, context, args)
+  COMMON.DungeonInteract(context.User, context.Target, context.CancelState, context.TurnCancel)
+end
+
+function BATTLE_SCRIPT.ShopkeeperInteract(owner, ownerChar, context, args)
+
+  if COMMON.CanTalk(context.Target) then
+	local security_state = COMMON.GetShopPriceState()
+    local price = security_state.Cart
+    local sell_price = COMMON.GetDungeonSellPrice()
+  
+    local oldDir = context.Target.CharDir
+    DUNGEON:CharTurnToChar(context.Target, context.User)
+	
+    if sell_price > 0 then
+      context.TurnCancel.Cancel = true
+      UI:SetSpeaker(context.Target)
+	  UI:ChoiceMenuYesNo(STRINGS:Format(RogueEssence.StringKey(string.format("TALK_SHOP_SELL_%04d", context.Target.Discriminator)):ToLocal(), STRINGS:FormatKey("MONEY_AMOUNT", sell_price)), false)
+	  UI:WaitForChoice()
+	  result = UI:ChoiceResult()
+	  
+	  if SV.adventure.Thief then
+	    COMMON.ThiefReturn()
+	  elseif result then
+	    -- iterate player inventory prices and remove total price
+        COMMON.PayDungeonSellPrice(sell_price)
+	    SOUND:PlayBattleSE("DUN_Money")
+	    UI:WaitShowDialogue(RogueEssence.StringKey(string.format("TALK_SHOP_SELL_DONE_%04d", context.Target.Discriminator)):ToLocal())
+	  else
+	    -- nothing
+	  end
+    end
+	
+    if price > 0 then
+      context.TurnCancel.Cancel = true
+      UI:SetSpeaker(context.Target)
+	  UI:ChoiceMenuYesNo(STRINGS:Format(RogueEssence.StringKey(string.format("TALK_SHOP_PAY_%04d", context.Target.Discriminator)):ToLocal(), STRINGS:FormatKey("MONEY_AMOUNT", price)), false)
+	  UI:WaitForChoice()
+	  result = UI:ChoiceResult()
+	  if SV.adventure.Thief then
+	    COMMON.ThiefReturn()
+	  elseif result then
+	    if price > GAME:GetPlayerMoney() then
+          UI:WaitShowDialogue(RogueEssence.StringKey(string.format("TALK_SHOP_PAY_SHORT_%04d", context.Target.Discriminator)):ToLocal())
+	    else
+	      -- iterate player inventory prices and remove total price
+          COMMON.PayDungeonCartPrice(price)
+	      SOUND:PlayBattleSE("DUN_Money")
+	      UI:WaitShowDialogue(RogueEssence.StringKey(string.format("TALK_SHOP_PAY_DONE_%04d", context.Target.Discriminator)):ToLocal())
+	    end
+	  else
+	    UI:WaitShowDialogue(RogueEssence.StringKey(string.format("TALK_SHOP_PAY_REFUSE_%04d", context.Target.Discriminator)):ToLocal())
+	  end
+    end
+	
+	if price == 0 and sell_price == 0 then
+      context.CancelState.Cancel = true
+      UI:SetSpeaker(context.Target)
+      UI:WaitShowDialogue(RogueEssence.StringKey(string.format("TALK_SHOP_%04d", context.Target.Discriminator)):ToLocal())
+      context.Target.CharDir = oldDir
+    end
+  else
+
+    UI:ResetSpeaker()
+	
+	local chosen_quote = RogueEssence.StringKey("TALK_CANT"):ToLocal()
+    chosen_quote = string.gsub(chosen_quote, "%[myname%]", context.Target:GetDisplayName(true))
+    UI:WaitShowDialogue(chosen_quote)
+  end
+end
+
+function BATTLE_SCRIPT.EscortInteract(owner, ownerChar, context, args)
+  context.CancelState.Cancel = true
+  local oldDir = context.Target.CharDir
+  DUNGEON:CharTurnToChar(context.Target, context.User)
+  UI:SetSpeaker(context.Target)
+  UI:WaitShowDialogue("I'm counting on you!")
+  context.Target.CharDir = oldDir
+end
+
+function BATTLE_SCRIPT.RescueReached(owner, ownerChar, context, args)
+
+  local tbl = LTBL(context.Target)
+  local mission = SV.test_grounds.Missions[tbl.Mission]
+  mission.Complete = 1
+  
+  local oldDir = context.Target.CharDir
+  DUNGEON:CharTurnToChar(context.Target, context.User)
+  
+  UI:SetSpeaker(context.Target)
+  UI:WaitShowDialogue("Yay, you found me!")
+  
+  -- warp out
+  TASK:WaitTask(_DUNGEON:ProcessBattleFX(context.Target, context.Target, _DATA.SendHomeFX))
+  _DUNGEON:RemoveChar(context.Target)
+  
+  UI:ResetSpeaker()
+  UI:WaitShowDialogue("Mission status set to complete. Return to quest giver for reward.")
+end
+
+
+function BATTLE_SCRIPT.EscortRescueReached(owner, ownerChar, context, args)
+  
+  local tbl = LTBL(context.Target)
+  local escort = COMMON.FindMissionEscort(tbl.Mission)
+  
+  if escort then
+    
+    local mission = SV.test_grounds.Missions[tbl.Mission]
+    mission.Complete = 1
+  
+    local oldDir = context.Target.CharDir
+    DUNGEON:CharTurnToChar(context.Target, context.User)
+  
+    UI:SetSpeaker(context.Target)
+    UI:WaitShowDialogue("Yay, you brought the escort to me!")
+  
+    -- warp out
+    TASK:WaitTask(_DUNGEON:ProcessBattleFX(escort, escort, _DATA.SendHomeFX))
+    _DUNGEON:RemoveChar(escort)
+	
+    TASK:WaitTask(_DUNGEON:ProcessBattleFX(context.Target, context.Target, _DATA.SendHomeFX))
+    _DUNGEON:RemoveChar(context.Target)
+  
+    UI:ResetSpeaker()
+    UI:WaitShowDialogue("Mission status set to complete. Return to quest giver for reward.")
+  end
+end
+
+function BATTLE_SCRIPT.CountTalkTest(owner, ownerChar, context, args)
+  context.CancelState.Cancel = true
+  
+  local tbl = LTBL(context.Target)
+  
+  local oldDir = context.Target.CharDir
+  DUNGEON:CharTurnToChar(context.Target, context.User)
+  
+  UI:SetSpeaker(context.Target)
+  
+  if tbl.TalkAmount == nil then
+    UI:WaitShowDialogue("I will remember how many times I've been talked to.")
+	tbl.TalkAmount = 1
+  else
+	tbl.TalkAmount = tbl.TalkAmount + 1
+  end
+  UI:WaitShowDialogue("You've talked to me "..tostring(tbl.TalkAmount).." times.")
+  
+  context.Target.CharDir = oldDir
+end
+
+
+function BATTLE_SCRIPT.PairTalk(owner, ownerChar, context, args)
+  context.CancelState.Cancel = true
+  
+  local oldDir = context.Target.CharDir
+  DUNGEON:CharTurnToChar(context.Target, context.User)
+  
+  UI:SetSpeaker(context.Target)
+  
+  if args.Pair == 0 then
+    UI:WaitShowDialogue(STRINGS:Format(RogueEssence.StringKey("TALK_ADVICE_TEAM_MODE"):ToLocal(), _DIAG:GetControlString(RogueEssence.FrameInput.InputType.TeamMode)))
+  else
+    if _DIAG.GamePadActive then
+	  UI:WaitShowDialogue(STRINGS:Format(RogueEssence.StringKey("TALK_ADVICE_SWITCH_GAMEPAD"):ToLocal(), _DIAG:GetControlString(RogueEssence.FrameInput.InputType.LeaderSwapBack), _DIAG:GetControlString(RogueEssence.FrameInput.InputType.LeaderSwapForth)))
+	else
+	  UI:WaitShowDialogue(STRINGS:Format(RogueEssence.StringKey("TALK_ADVICE_SWITCH_KEYBOARD"):ToLocal(), _DIAG:GetControlString(RogueEssence.FrameInput.InputType.LeaderSwap1), _DIAG:GetControlString(RogueEssence.FrameInput.InputType.LeaderSwap2), _DIAG:GetControlString(RogueEssence.FrameInput.InputType.LeaderSwap3), _DIAG:GetControlString(RogueEssence.FrameInput.InputType.LeaderSwap4)))
+	end
+  end
+  
+  
+  context.Target.CharDir = oldDir
+end
+
+
+
+
---
+++ Data/Script/event_single.lua
@@ -0,0 +1,201 @@
+require 'common'
+
+SINGLE_CHAR_SCRIPT = {}
+
+function SINGLE_CHAR_SCRIPT.Test(owner, ownerChar, character, args)
+  PrintInfo("Test")
+end
+
+
+ShopSecurityType = luanet.import_type('PMDC.Dungeon.ShopSecurityState')
+MapIndexType = luanet.import_type('RogueEssence.Dungeon.MapIndexState')
+
+
+function SINGLE_CHAR_SCRIPT.ThiefCheck(owner, ownerChar, character, args)
+  local baseLoc = _DUNGEON.ActiveTeam.Leader.CharLoc
+  local tile = _ZONE.CurrentMap.Tiles[baseLoc.X][baseLoc.Y]
+  
+  local thief_idx = 31
+  
+  local price = COMMON.GetDungeonCartPrice()
+  local security_price = COMMON.GetShopPriceState()
+  if price < 0 then
+    --lost merchandise was placed back in shop, readjust the security price and clear the current price
+    security_price.Amount = security_price.Amount - price
+  elseif price < security_price.Cart then
+    --merchandise was returned.  doesn't matter who did it.
+    security_price.Cart = price
+  elseif price > security_price.Cart then
+    local char_index = _ZONE.CurrentMap:GetCharIndex(character)
+    if char_index.Faction ~= RogueEssence.Dungeon.Faction.Player then
+      --non-player was responsible for taking/destroying merchandise, just readjust the security price and clear the current price
+      security_price.Amount = security_price.Amount - price + security_price.Cart
+	else
+	  --player was responsible for taking/destroying merchandise, add to the cart
+      security_price.Cart = price
+    end
+  end
+
+  
+  if tile.Effect.ID ~= 45 then
+	if security_price.Cart > 0 then
+	  _GAME:BGM("", false)
+      COMMON.ClearAllPrices()
+	  
+	  SV.adventure.Thief = true
+	  local index_from = owner.StatusStates:Get(luanet.ctype(MapIndexType))
+	  _DUNGEON:LogMsg(RogueEssence.StringKey(string.format("TALK_SHOP_THIEF_%04d", index_from.Index)):ToLocal())
+		
+	  -- create thief status
+	  local thief_status = RogueEssence.Dungeon.MapStatus(thief_idx)
+      thief_status:LoadFromData()
+	  -- put spawner from security status in thief status
+	  local security_to = thief_status.StatusStates:Get(luanet.ctype(ShopSecurityType))
+	  local security_from = owner.StatusStates:Get(luanet.ctype(ShopSecurityType))
+	  security_to.Security = security_from.Security
+      TASK:WaitTask(_DUNGEON:RemoveMapStatus(owner.ID))
+      TASK:WaitTask(_DUNGEON:AddMapStatus(thief_status))
+	  GAME:WaitFrames(60)
+	end
+  else
+    local shop_idx = 37
+	if not _ZONE.CurrentMap.Status:ContainsKey(thief_idx) and not _ZONE.CurrentMap.Status:ContainsKey(shop_idx) then
+	  
+	  local shop_status = RogueEssence.Dungeon.MapStatus(shop_idx)
+      shop_status:LoadFromData()
+      TASK:WaitTask(_DUNGEON:AddMapStatus(shop_status))
+	end
+  end
+end
+
+function SINGLE_CHAR_SCRIPT.ShopCheckout(owner, ownerChar, character, args)
+  local baseLoc = _DUNGEON.ActiveTeam.Leader.CharLoc
+  local tile = _ZONE.CurrentMap.Tiles[baseLoc.X][baseLoc.Y]
+
+  if tile.Effect.ID ~= 45 then
+	local found_shopkeep = COMMON.FindNpcWithTable(false, "Role", "Shopkeeper")
+    if found_shopkeep and COMMON.CanTalk(found_shopkeep) then
+	  local security_state = COMMON.GetShopPriceState()
+      local price = security_state.Cart
+	  local sell_price = COMMON.GetDungeonSellPrice()
+  
+      if price > 0 or sell_price > 0 then
+	    local is_near = false
+		local loc_diff = _DUNGEON.ActiveTeam.Leader.CharLoc - found_shopkeep.CharLoc
+		if loc_diff:Dist8() > 1 then
+		  -- check to see if the shopkeeper can see the player and warp there
+		  local near_mat = false
+		  local dirs = { Direction.Down, Direction.DownLeft, Direction.Left, Direction.UpLeft, Direction.Up, Direction.UpRight, Direction.Right, Direction.DownRight }
+		  for idx, dir in pairs(dirs) do
+            if COMMON.ShopTileCheck(baseLoc, dir) then
+		      near_mat = true
+		    end
+		  end
+		  
+		  if (near_mat or found_shopkeep:CanSeeCharacter(_DUNGEON.ActiveTeam.Leader)) then
+	        -- attempt to warp the shopkeeper next to the player
+		    local cand_locs = _ZONE.CurrentMap:FindNearLocs(found_shopkeep, baseLoc, 1)
+		    if cand_locs.Count > 0 then
+		      TASK:WaitTask(_DUNGEON:PointWarp(found_shopkeep, cand_locs[0], false))
+			  is_near = true
+		    end
+		  end
+		else
+		  is_near = true
+		end
+
+		-- if it can't, fall through
+		if is_near then
+		  DUNGEON:CharTurnToChar(found_shopkeep, _DUNGEON.ActiveTeam.Leader)
+		  DUNGEON:CharTurnToChar(_DUNGEON.ActiveTeam.Leader, found_shopkeep)
+          UI:SetSpeaker(found_shopkeep)
+		  
+		  if sell_price > 0 then
+		    UI:ChoiceMenuYesNo(STRINGS:Format(RogueEssence.StringKey(string.format("TALK_SHOP_SELL_%04d", found_shopkeep.Discriminator)):ToLocal(), STRINGS:FormatKey("MONEY_AMOUNT", sell_price)), false)
+		    UI:WaitForChoice()
+		    result = UI:ChoiceResult()
+		  
+		    if SV.adventure.Thief then
+			  COMMON.ThiefReturn()
+		    elseif result then
+			  -- iterate player inventory prices and remove total price
+			  COMMON.PayDungeonSellPrice(sell_price)
+			  SOUND:PlayBattleSE("DUN_Money")
+			  UI:WaitShowDialogue(RogueEssence.StringKey(string.format("TALK_SHOP_SELL_DONE_%04d", found_shopkeep.Discriminator)):ToLocal())
+		    else
+			  -- nothing
+		    end
+		  end
+		  
+		  if price > 0 then
+	        UI:ChoiceMenuYesNo(STRINGS:Format(RogueEssence.StringKey(string.format("TALK_SHOP_PAY_%04d", found_shopkeep.Discriminator)):ToLocal(), STRINGS:FormatKey("MONEY_AMOUNT", price)), false)
+	        UI:WaitForChoice()
+	        result = UI:ChoiceResult()
+	        if SV.adventure.Thief then
+			  COMMON.ThiefReturn()
+		    elseif result then
+	          if price > GAME:GetPlayerMoney() then
+                UI:WaitShowDialogue(RogueEssence.StringKey(string.format("TALK_SHOP_PAY_SHORT_%04d", found_shopkeep.Discriminator)):ToLocal())
+	          else
+	            -- iterate player inventory prices and remove total price
+                COMMON.PayDungeonCartPrice(price)
+		        SOUND:PlayBattleSE("DUN_Money")
+	            UI:WaitShowDialogue(RogueEssence.StringKey(string.format("TALK_SHOP_PAY_DONE_%04d", found_shopkeep.Discriminator)):ToLocal())
+	          end
+	        end
+		  end
+		end
+      else
+        UI:SetSpeaker(found_shopkeep)
+        UI:WaitShowDialogue(RogueEssence.StringKey(string.format("TALK_SHOP_END_%04d", found_shopkeep.Discriminator)):ToLocal())
+      end
+	end
+  end
+end
+
+function SINGLE_CHAR_SCRIPT.DestinationFloor(owner, ownerChar, character, args)
+  SOUND:PlayFanfare("Fanfare/Note")
+  UI:ResetSpeaker()
+  UI:WaitShowDialogue("You've reached a destination floor!")
+end
+
+
+function SINGLE_CHAR_SCRIPT.OutlawFloor(owner, ownerChar, character, args)
+  SOUND:PlayBGM("C07. Outlaw.ogg", false)
+  UI:ResetSpeaker()
+  UI:WaitShowDialogue("Wanted outlaw spotted!")
+  
+  -- add a map status for outlaw clear check
+  local checkClearStatus = 35 -- outlaw clear check
+  local status = RogueEssence.Dungeon.MapStatus(checkClearStatus)
+  status:LoadFromData()
+  TASK:WaitTask(_DUNGEON:AddMapStatus(status))
+end
+
+function SINGLE_CHAR_SCRIPT.OutlawClearCheck(owner, ownerChar, character, args)
+  -- check for no outlaw in the mission list
+  remaining_outlaw = false
+  for name, mission in pairs(SV.test_grounds.Missions) do
+    PrintInfo("Checking Mission: "..tostring(name))
+    if mission.Complete == COMMON.MISSION_INCOMPLETE and _ZONE.CurrentZoneID == mission.DestZone
+	  and _ZONE.CurrentMapID.Segment == mission.DestSegment and _ZONE.CurrentMapID.ID == mission.DestFloor then
+	  local found_outlaw = COMMON.FindNpcWithTable(true, "Mission", name)
+      if found_outlaw then
+	    remaining_outlaw = true
+	  else
+	    -- if no outlaws of the mission list, mark quest as complete
+		mission.Complete = 1
+		UI:ResetSpeaker()
+        UI:WaitShowDialogue("Mission status set to complete. Return to quest giver for reward.")
+	  end
+    end
+  end
+  if not remaining_outlaw then
+    -- if no outlaws are found in the map, return music to normal and remove this status from the map
+    SOUND:PlayBGM(_ZONE.CurrentMap.Music, true)
+    local checkClearStatus = 35 -- outlaw clear check
+	TASK:WaitTask(_DUNGEON:RemoveMapStatus(checkClearStatus))
+  end
+end
+
+

04fb1d4d3504d99111d13ecd58ccfa0041afa259
audino <2676737+audinowho@users.noreply.github.com>
2022-06-30 22:02:10 -0800

    Fix gifting inventory overflow

--- Data/Script/common.lua
+++ Data/Script/common.lua
@@ -244,7 +244,7 @@ function COMMON.GiftItemFull(player, receive_item, fanfare, force_storage)
   end
   UI:ResetSpeaker(false)
   UI:SetCenter(true)
-  if not force_storage and GAME:GetPlayerBagCount() < GAME:GetPlayerBagLimit() then
+  if not force_storage and GAME:GetPlayerBagCount() + GAME:GetPlayerEquippedCount() < GAME:GetPlayerBagLimit() then
     --give to inventory
 	UI:WaitShowDialogue(STRINGS:Format(RogueEssence.StringKey("DLG_RECEIVE_ITEM"):ToLocal(), player:GetDisplayName(), receive_item:GetDisplayName()))
 	GAME:GivePlayerItem(receive_item)

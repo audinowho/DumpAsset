daab5db64f84b52de8cb024f8cbe3037b7000e7c
audino <2676737+audinowho@users.noreply.github.com>
2023-01-30 22:47:18 -0800

    coroutine item event

--- Data/Script/event_misc.lua
+++ Data/Script/event_misc.lua
@@ -55,9 +55,10 @@ end
 ITEM_SCRIPT = {}
 
 function ITEM_SCRIPT.Test(owner, ownerChar, context, args)
+  PrintInfo("Test")
   local text = "You got a " .. context.Item:GetDungeonName()
   local notice = _MENU:CreateNotice("Test", text)
-  _DUNGEON.PendingLeaderAction = _MENU:ProcessMenuCoroutine(notice)
+  TASK:WaitTask(_MENU:ProcessMenuCoroutine(notice))
 end
 
 REFRESH_SCRIPT = {}

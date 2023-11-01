7b82f76808fc34ba8a8f2dff52fa0d604a093810
audino <2676737+audinowho@users.noreply.github.com>
2023-10-31 16:12:25 -0800

    pull request revisions

--- Data/Script/recruit_list.lua
+++ Data/Script/recruit_list.lua
@@ -21,10 +21,6 @@ RECRUIT_LIST = {}
 -- -----------------------------------------------
 -- Constants
 -- -----------------------------------------------
-RECRUIT_LIST.recruitMenuOption = "Recruitment search"
-RECRUIT_LIST.menuTitle = "Potential Recruits"
-RECRUIT_LIST.noRecruitsMessage = "No recruits available"
-
 
 RECRUIT_LIST.hide =                    0
 RECRUIT_LIST.undiscovered =            1
@@ -45,11 +41,13 @@ RECRUIT_LIST.colorList = {'#FFFFFF', '#FFFFFF', '#00FFFF', '#FFFF00', '#FFFFA0',
 -- string is replaced with "???" before wrapping
 function RECRUIT_LIST.colorName(monster, mode, asterisk)
     local name = _DATA:GetMonster(monster).Name:ToLocal()
-    if asterisk then name = "*"..name end -- asterisk is for mons that are not in the spawn list but spawned at floor start
-    if mode == 1 then name = '???' end
+    if asterisk then name = "\u{E111}"..name end -- asterisk is for mons that are not in the spawn list but spawned at floor start
+    if mode == RECRUIT_LIST.undiscovered then name = '???' end
     local color = RECRUIT_LIST.colorList[mode]
     return '[color='..color..']'..name..'[color]'
 end
+
+UnrecruitableType = luanet.import_type('PMDC.LevelGen.MobSpawnUnrecruitable')
 
 -- Extracts and sorts the list of all mons that are spawned at the start of the current floor
 -- Non-respawning mons will have an asterisk at the start of their name
@@ -78,14 +76,13 @@ function RECRUIT_LIST.compileInitialFloorList()
             -- check if the mon is recruitable
             local features = spawnList:GetSpawn(j).SpawnFeatures
             for f = 0, features.Count-1, 1 do
-                if RECRUIT_LIST.getClass(features[f]) == "PMDC.LevelGen.MobSpawnUnrecruitable" then
+                if LUA_ENGINE:TypeOf(features[f]) == luanet.ctype(UnrecruitableType) then
                     hide = true -- do not show in recruit list if cannot recruit
                 end
             end
 
             -- add the member and its display mode to the list
             if not hide and list.entries[member] == nil then
-                print("added "..member, false)
                 table.insert(list.keys, member)
                 list.entries[member] = false
             end
@@ -105,7 +102,6 @@ function RECRUIT_LIST.compileInitialFloorList()
 
             -- add the member and its display mode to the list
             if not hide and list.entries[member] == nil then
-                print("added "..member, true)
                 table.insert(list.keys, member)
                 list.entries[member] = true
             end
@@ -121,58 +117,9 @@ function RECRUIT_LIST.compileInitialFloorList()
 end
 
 
--- Extracts a list of all mons currently spawned on the current floor and
--- then appends them at the end of the spawn list, all while pairing them to
--- the display mode that should be used for that mon's name in the menu
--- output: a new list of table entries containing these properties:RECRUIT_LIST
--- {string species, int mode, boolean asterisk}
 function RECRUIT_LIST.compileCurrentList()
-    SV.temp = SV.temp or {}
-    local list = SV.temp.floorList or {
-        keys = {},
-        entries = {}
-    }
-
-    local new_list = {
-        keys = {},
-        entries = {}
-    }
-    -- abort immediately if we're not inside a dungeon
-    if RogueEssence.GameManager.Instance.CurrentScene ~= RogueEssence.Dungeon.DungeonScene.Instance then
-        return new_list
-    end
-
-    local map = _ZONE.CurrentMap
-    local teams = map.MapTeams
-
-    for i = 0, teams.Count-1, 1 do
-        local team = teams[i].Players
-        for j = 0, team.Count-1, 1 do
-            local member = team[j].BaseForm.Species
-            local state = _DATA.Save:GetMonsterUnlock(member)
-            local mode = RECRUIT_LIST.hide -- default is to not show non-respawning mons if unknown
-
-            -- check if the mon has been seen or obtained
-            if state == RogueEssence.Data.GameProgress.UnlockState.Discovered then
-                mode = RECRUIT_LIST.extra_discovered
-            elseif state == RogueEssence.Data.GameProgress.UnlockState.Completed then
-                if _DATA:GetMonster(member).Forms.Count>1 then
-                    mode = RECRUIT_LIST.extra_obtainedMultiForm
-                else
-                    mode = RECRUIT_LIST.extra_obtained
-                end
-            end
-            -- do not show in recruit list if cannot recruit
-            if team[j].Unrecruitable then mode = RECRUIT_LIST.hide end
-
-            -- add the member and its display mode to the list if it's not in any of them already
-            if mode > RECRUIT_LIST.hide and list.entries[member] == nil and new_list.entries[member] == nil then
-                print("found "..member, false)
-                table.insert(new_list.keys, member)
-                new_list.entries[member] = {mode, false}
-            end
-        end
-    end
+    
+    local list = RECRUIT_LIST.compileInitialFloorList()
 
     -- turn first list into final output
     local ret = {}
@@ -184,11 +131,7 @@ function RECRUIT_LIST.compileCurrentList()
         if state == RogueEssence.Data.GameProgress.UnlockState.Discovered then
             mode = RECRUIT_LIST.discovered
         elseif state == RogueEssence.Data.GameProgress.UnlockState.Completed then
-            if _DATA:GetMonster(key).Forms.Count>1 then
-                mode = RECRUIT_LIST.obtainedMultiForm --special color for multi-form mons
-            else
-                mode = RECRUIT_LIST.obtained
-            end
+            mode = RECRUIT_LIST.obtained
         end
 
         local entry = {
@@ -199,27 +142,9 @@ function RECRUIT_LIST.compileCurrentList()
         table.insert(ret,entry)
     end
 
-    -- put new list into final output
-    for _,key in pairs(new_list.keys) do
-        local entry = {
-            species = key,
-            mode = new_list.entries[key][1],
-            asterisk = new_list.entries[key][2]
-        }
-        table.insert(ret,entry)
-    end
     return ret
 end
 
--- Returns the class of an object as string. Useful to extract and check C# classes
-function RECRUIT_LIST.getClass(csobject)
-    if not csobject then return "nil" end
-    local namet = getmetatable(csobject).__name
-    if not namet then return type(csobject) end
-    for a in namet:gmatch('([^,]+)') do
-        return a
-    end
-end
 
 -- Returns the class of an object as string. Useful to extract and check C# classes
 function RECRUIT_LIST.tri(check, y, n)
@@ -254,11 +179,11 @@ function RecruitmentListMenu:DrawMenu()
     --Standard menu divider. Reuse this whenever you need a menu divider at the top for a title.
     self.menu.MenuElements:Add(RogueEssence.Menu.MenuDivider(RogueElements.Loc(8, 8 + 12), self.menu.Bounds.Width - 8 * 2))
 
-    self.menu.MenuElements:Add(RogueEssence.Menu.MenuText(RECRUIT_LIST.menuTitle, RogueElements.Loc(16, 8)))
+    self.menu.MenuElements:Add(RogueEssence.Menu.MenuText(RogueEssence.StringKey("MENU_RECRUITMENT_TITLE"):ToLocal(), RogueElements.Loc(16, 8)))
 
     -- add a special message if there are no entries
     if #self.list<1 then
-        self.menu.MenuElements:Add(RogueEssence.Menu.MenuText(RECRUIT_LIST.noRecruitsMessage, RogueElements.Loc(16, 24)))
+        self.menu.MenuElements:Add(RogueEssence.Menu.MenuText(RogueEssence.StringKey("MENU_RECRUITMENT_NONE"):ToLocal(), RogueElements.Loc(16, 24)))
         return
     end
 
@@ -281,11 +206,11 @@ function RecruitmentListMenu:DrawMenu()
         -- positional parameters
         local line = count % self.ENTRY_LINES
         local col = count // self.ENTRY_LINES
-        local xpad = 16
+        local xpad = 24
         local ypad = 24
         local xdist = ((self.menu.Bounds.Width-32)//self.ENTRY_COLUMNS)
         local ydist = 14
-        local xadjust = RECRUIT_LIST.tri(self.list[i].asterisk and self.list[i].mode > RECRUIT_LIST.undiscovered, 6, 0)
+        local xadjust = RECRUIT_LIST.tri(self.list[i].asterisk and self.list[i].mode > RECRUIT_LIST.undiscovered, 10, 0)
 
         -- add element
         local x = xpad + xdist * col - xadjust

437f57a3964fc612c2f882fe448d52ef9d4311c7
Touhou <122487411+leo-in-the-house@users.noreply.github.com>
2024-01-15 13:24:03 -0600

    Remove test Mysteriosity function

--- Data/Script/event_mapgen.lua
+++ Data/Script/event_mapgen.lua
@@ -21,10 +21,6 @@ PresetPickerType = luanet.import_type('RogueElements.PresetPicker`1')
 MapItemType = luanet.import_type('RogueEssence.Dungeon.MapItem')
 
 
-function ZONE_GEN_SCRIPT.Mysteriosity(zoneContext, context, queue, seed, args)
-  PrintInfo("Test")
-end
-
 function ZONE_GEN_SCRIPT.GenerateMissionFromSV(zoneContext, context, queue, seed, args)
   SV.DestinationFloorNotified = false
   SV.MonsterHouseMessageNotified = false
